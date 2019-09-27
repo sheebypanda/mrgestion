f94a16b43cf62c0cb5da5c059a86ba855f4ee136
sheebypanda <sheebypanda@gmail.com>
2019-09-27 14:39:08 +0200

    fix devise bug after disabling registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
   resources :jourferies, :intemperies, :prestations, :machines, :clients, :employeurs
   resources :factures, :facture_lignes
 
